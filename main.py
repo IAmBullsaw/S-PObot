b45f894c47a67d958d18e0f745d0f9b98ca2f28d
Oskar Jansson <oskar@oskarjansson.com>
2019-06-28 19:55:44 +0200

    updates: better retries and logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
-import praw
+import sys
 from prawcore import ServerError
+import praw
 
 
 def get_sweddit():
@@ -8,6 +9,7 @@ def get_sweddit():
     Creates the Reddit instance
     :return: the subreddit /r/sweden
     """
+    # FIXME: Seems like this does not properly authenticate/log in.
     reddit = praw.Reddit(site_name='S-PO')
     sub = reddit.subreddit('sweden')
     return sub
@@ -23,21 +25,27 @@ def get_submissions(sweddit, retries=5, wait=10, limit=10):
     :return: the amount of submissions from sweddits hot up to limit
     """
     submissions = sweddit.hot(limit=limit)
+    successful = False
     while retries:
         retries -= 1
         try:
             submissions = next(submissions)
+            successful = True
             break
         except ServerError as e:
-            print(e.__str__(), 'will retry in %d seconds:' % limit, retries)
+            print('Received', e.response, '... retry in %d seconds:' % wait, retries, file=sys.stderr)
             time.sleep(wait)
-    return submissions
+    return submissions if successful else None
 
 
 def main():
     sweddit = get_sweddit()
 
     submissions = get_submissions(sweddit)
+    if not submissions:
+        print("[ERROR] Didn't get any submissions ... ", file=sys.stderr)
+        exit(1)
+
     for submission in submissions:
         print(submission.title)
 
